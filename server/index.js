82cdb65777b4a8f0a475b801dccb2b1778f04a9b
Lee Joramo <lee.joramo@d51schools.org>
2022-01-28 16:33:06 -0700

    ADD CORS

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@
 // external packages
 
 const express = require('express')
+const cors = require('cors')
 const bodyParser = require('body-parser')
 const flash = require('connect-flash')
 const session = require('../lib/sessions')
@@ -26,6 +27,16 @@ module.exports = function() {
         server.set('port', config.port)
         server.set('hostname', config.hostname)
         server.set('viewDir', config.viewDir)
+
+        // use CORS
+
+        server.use(cors())
+
+        server.use(function(req, res, next) {
+            res.header("Access-Control-Allow-Origin", "*")
+            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+            next()
+        });
 
         // Returns middleware that parses json
         server.use(bodyParser.urlencoded({ extended: false }))
@@ -61,3 +72,6 @@ module.exports = function() {
         start: start
     }
 }
+
+
+
